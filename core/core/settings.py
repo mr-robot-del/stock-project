399bdc02e3a6b194b3be235d1c507b220dd818d9
mr-robot-del <frenzyaboy4life@gmail.com>
2025-10-23 23:22:36 +0100

    Add Alpha Vantage API key to settings and enhance admin models for Stock and StockData

--- core/core/settings.py
+++ core/core/settings.py
@@ -22,6 +22,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+ALPHA_VANTAGE_API_KEY = os.getenv('ALPHA_VANTAGE_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'rest_framework',
+    'rest_framework.authtoken',
     'stocks',
     'analysis',
     'portfolios',
--- core/stocks/admin.py
+++ core/stocks/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Stock, StockData, Prediction
 
-# Register your models here.
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ['ticker', 'name', 'sector']
+    search_fields = ['ticker', 'name']
+
+@admin.register(StockData)
+class StockDataAdmin(admin.ModelAdmin):
+    list_display = ['stock', 'date', 'close', 'volume']
+    list_filter = ['stock', 'date']
+    search_fields = ['stock__ticker']
+
+@admin.register(Prediction)
+class PredictionAdmin(admin.ModelAdmin):
+    list_display = ['stockdata', 'moving_average', 'computed_at']
+    list_filter = ['computed_at']
